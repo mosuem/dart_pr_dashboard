b49249657953655545ddaae742b392363eb6437e
Moritz <mosum@google.com>
2024-02-14 08:49:08 +0100

    Fix bug

--- pkgs/dart_triage_updater/lib/dart_triage_updater.dart
+++ pkgs/dart_triage_updater/lib/dart_triage_updater.dart
@@ -73,8 +73,8 @@ class TriageUpdater {
     final reviewers = await github.pullRequests
         .listReviews(slug, pr.number!)
         .map((prReview) => prReview.user)
-        // .where((user) => user != null) // Uncomment when https://github.com/SpinlockLabs/github.dart/pull/383 is merged
-        // .map((user) => user!)
+        .where((user) => user != null)
+        .map((user) => user!)
         .toList();
     await wait();
     // Deduplicate reviewers
